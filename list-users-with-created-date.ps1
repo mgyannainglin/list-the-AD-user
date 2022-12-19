62abac54fcb54dc0a21e37e661285fa9c26f0b06
Yan Naing Lin <61822567+mgyannainglin@users.noreply.github.com>
2022-12-19 16:46:42 +0800

    Add files via upload

--- list-users-with-created-date.ps1
+++ list-users-with-created-date.ps1
@@ -1,2 +1,2 @@
-Get-ADUser -Filter “Enabled -eq 'true'” -Properties whenCreated | Select Name,whenCreated | Export-Csv -Path d:\samaccountname.csv -NoTypeInformation
+Get-ADUser -Filter “Enabled -eq 'true'” -Properties whenCreated | Select Name,whenCreated | Export-Csv -Path d:\userlist.csv -NoTypeInformation
 
